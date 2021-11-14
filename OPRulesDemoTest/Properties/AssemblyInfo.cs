7326a138eb09eac033025d542e37464866eb51ca
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 23:50:16 +0100

    Added test case

---
+++ OPRulesDemoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OPRulesDemoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OPRulesDemoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fcffbc2f-a7b5-4642-8cf7-34d3ae9c3f02")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OPRulesDemoTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OPRulesDemo.Entity;
+
+namespace OPRulesDemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnVideoLearningToSkiSlipOnly()
+        {
+            var result = Orders.CompleteOrder(new string[] { "video", "Learning To Ski" });
+            Assert.AreEqual("Learning To Ski", result.Name);
+            Assert.AreEqual("Generated a packing slip.", result.CheckoutProcess[0]);
+            Assert.AreEqual("'First Aid' video added to the packing slip", result.CheckoutProcess[1]);
+            Assert.AreEqual(2, result.CheckoutProcess.Count);
+        }
+    }
+}
