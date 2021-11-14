c744010965f1cdd29e9777b6303befaaca6d954e
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 22:47:19 +0100

    Added new class for nonphysical product

--- Entity/NonPhysicalProduct.cs
+++ Entity/NonPhysicalProduct.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace OPRulesDemo.Entity
 {
-    public abstract class PhysicalProduct : Product
+    public abstract class NonPhysicalProduct : Product
     {
         public override void GenerateCustomerSlip()
         {
             CheckoutProcess.Add("Generate a packing slip for shipping.");
             Console.WriteLine("Generate a packing slip for shipping.");
         }
-        public virtual void AddCommission()
+        public virtual void SendEmail()
         {
-            CheckoutProcess.Add("Commission generated for agent");
-            Console.WriteLine("Commission generated for agent");
+            CheckoutProcess.Add("Mail Sent to customer");
+            Console.WriteLine("Mail Sent to customer");
         }
     }
 }
