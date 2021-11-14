4c10465175e52adbf97effa641e32bb7f7fa99c6
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 23:19:34 +0100

    calling object of orders in main fucntion

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OPRulesDemo.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,12 @@ namespace OPRulesDemo
     {
         static void Main(string[] args)
         {
-
+            Orders order = new Orders();
+            Console.WriteLine("Enter Product type and name ");
+            var input = Console.ReadLine().Split(' ');
+            var output = Orders.CompleteOrder(input);
+            //Console.WriteLine("Item Name : {0} Operations : {1}", output.Name, string.Join(" ", output.CheckoutProcess));
+            Console.ReadLine();
         }
     }
 }
