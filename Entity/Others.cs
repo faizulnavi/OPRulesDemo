43f45cc92afbdfd85346f72c1f95f9df4e8608d0
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 23:08:21 +0100

    Added two classes to handle product type and others

---
+++ Entity/Others.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPRulesDemo.Entity
+{
+    class Others : PhysicalProduct
+    {
+        public Others(string name)
+        {
+            Name = name;
+            CheckoutProcess = new List<string>();
+            base.GenerateCustomerSlip();
+            base.AddCommission();
+        }
+    }
+}
