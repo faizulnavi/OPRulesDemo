ffcc0a24aadd021d18dffac8c8f8a10e8247cd38
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 23:16:12 +0100

    Added Orders class to handle orders

---
+++ Entity/Orders.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPRulesDemo.Entity
+{
+    class Orders
+    {
+        public static Product CompleteOrder(string[] input)
+        {
+            ProductType type;
+            try
+            {
+                type = (ProductType)Enum.Parse(typeof(ProductType), input[0], ignoreCase: true);
+            }
+            catch (Exception e)
+            {
+                type = ProductType.Other;
+            }
+            Product product;
+            string name = input.Length > 1 ? string.Join(" ", input, 1, input.Length - 1) : string.Empty;
+            switch (type)
+            {
+                case ProductType.Membership:
+                    {
+                        product = new Membership();
+                        break;
+                    }
+                case ProductType.Upgrade:
+                    {
+                        product = new Upgrade();
+                        break;
+                    }
+                case ProductType.Video:
+                    {
+                        product = new Videos(name);
+                        break;
+                    }
+                case ProductType.Book:
+                    {
+                        product = new Book(name);
+                        break;
+                    }
+                case ProductType.Other:
+                default:
+                    {
+                        product = new Others(name);
+                        break;
+                    }
+            }
+            return product;
+        }
+    }
+}
