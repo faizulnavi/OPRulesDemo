151cf6b211947288e100f863c7c5eb94ec2cb29a
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 22:36:41 +0100

    Added Book and PhysicalProduct classes

---
+++ Entity/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPRulesDemo.Entity
+{
+    class Book : PhysicalProduct
+    {
+        public Book(string productName)
+        {
+            Name = productName;
+            CheckoutProcess = new List<string>();
+            base.GenerateCustomerSlip();
+            base.AddCommission();
+            GenerateCustomerSlip();
+        }
+        public override void GenerateCustomerSlip()
+        {
+            CheckoutProcess.Add("Create a duplicate packing slip for the royalty department.");
+            Console.WriteLine("Create a duplicate packing slip for the royalty department.");
+        }
+    }
+}
---
+++ Entity/PhysicalProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPRulesDemo.Entity
+{
+    public abstract class PhysicalProduct : Product
+    {
+        public override void GenerateCustomerSlip()
+        {
+            CheckoutProcess.Add("Generated a packing slip for shipping.");
+            Console.WriteLine("Generated a packing slip for shipping.");
+        }
+        public virtual void AddCommission()
+        {
+            CheckoutProcess.Add("Commission payment to the agent");
+            Console.WriteLine("Commission payment to the agent");
+        }
+    }
+}
