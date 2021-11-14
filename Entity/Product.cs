0554ac877cf545851f624cf772bd24730127c61a
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 21:26:18 +0100

    Added new product class

---
+++ Entity/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPRulesDemo.Entity
+{
+    public abstract class Product
+    {
+        public string Name { get; set; }
+        public List<string> CheckoutProcess { get; set; }
+        public abstract void GenerateCustomerSlip();
+    }
+}
