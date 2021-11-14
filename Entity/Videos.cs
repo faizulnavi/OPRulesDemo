e809755af42aa1d9762c0b3329f996771cf3a4db
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 22:51:30 +0100

    Added new class to handle vidoes and video learning to ski

---
+++ Entity/Videos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPRulesDemo.Entity
+{
+    class Videos : NonPhysicalProduct
+    {
+        public Videos(string vName)
+        {
+            CheckoutProcess = new List<string>();
+            Name = vName;
+
+            GenerateCustomerSlip();
+        }
+        public override void GenerateCustomerSlip()
+        {
+            base.GenerateCustomerSlip();
+            if (Name.ToLower().Equals("learning to ski"))
+            {
+                CheckoutProcess.Add("'First Aid' added to the packing slip");
+                Console.WriteLine("'First Aid' added to the packing slip");
+            }
+        }
+    }
+}
