59e17d27cff37280178fd01d7bcbd33464fa6abe
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 22:59:57 +0100

    added class to handle upgrade

---
+++ Entity/Upgrade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPRulesDemo.Entity
+{
+    class Upgrade : NonPhysicalProduct
+    {
+        public Upgrade()
+        {
+            CheckoutProcess = new List<string>();
+            base.GenerateCustomerSlip();
+            CheckoutProcess.Add("Membership Upgraded");
+            Console.WriteLine("Membership Upgraded");
+            base.SendEmail();
+        }
+    }
+}
