5b23e65631871fa6c2d7a463f1738b359fdb5595
Faizul Navi Khan <faizulnavi@yahoo.co.in>
2021-11-14 22:55:05 +0100

    Added class to handle membership

---
+++ Entity/Membership.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPRulesDemo.Entity
+{
+    class Membership : NonPhysicalProduct
+    {
+        public Membership()
+        {
+            CheckoutProcess = new List<string>();
+            base.GenerateCustomerSlip();
+            CheckoutProcess.Add("Membership Activated");
+            Console.WriteLine("Membership Activated");
+            base.SendEmail();
+        }
+    }
+}
